124ea3b3376af65aa4ff406a84ee5e6ad6e2f47a
Bipul Kumar Singh <bksingh@netlink.com>
2021-03-16 10:15:08 +0530

    create and get user function added

--- blog/main.py
+++ blog/main.py
@@ -1,7 +1,9 @@
-from fastapi import FastAPI, Depends, status, HTTPException
+from typing import List
 from . import schemas, models
 from .database import engine, sessionLocal
 from sqlalchemy.orm import Session
+from .hashing import Hash
+from fastapi import FastAPI, Depends, status, HTTPException
 
 app = FastAPI()
 
@@ -45,15 +47,32 @@ def update(id: int, request: schemas.Blog, db: Session = Depends(get_db)):
     return {"message": "Blog updated"}
 
 
-@app.get('/blog')
+@app.get('/blog', response_model=List[schemas.ShowBlog])
 def all_blogs(db: Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
     return blogs
 
 
-@app.get('/blog/{id}', status_code=status.HTTP_200_OK)
+@app.get('/blog/{id}', status_code=status.HTTP_200_OK, response_model=schemas.ShowBlog)
 def single_blog(id: int, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
     if not blog:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with the id {id} is not available.")
     return blog
+
+
+@app.post('/user', response_model=schemas.ShowUser)
+def create_user(request: schemas.User, db: Session = Depends(get_db)):
+    new_user = models.User(name=request.name, email=request.email, password=Hash.bcrypt(request.password))
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
+
+
+@app.get('/user/{id}', response_model=schemas.ShowUser)
+def get_user(id: int, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == id).first()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"User with id {id} not found!")
+    return user
